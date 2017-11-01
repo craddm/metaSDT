92a42815360d21a134feb29f5a9b07dbfa0d1224
craddm <monklefish@gmail.com>
2017-11-01 22:37:18 +0000

    added additional tests and updated namespace

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,3 @@
 library(testthat)
+library(metaSDT)
 test_check("metaSDT")
--- tests/testthat/test_meta_d.R
+++ tests/testthat/test_meta_d.R
@@ -1,6 +1,5 @@
-library(metaSDT)
 
-context("Check meta-d MLE")
+context("Check meta-d measures")
 
 test_that("meta_d MLE gives expected result", {
   nR_S1 <- c(32, 23, 14, 9)
@@ -11,5 +10,19 @@ test_that("meta_d MLE gives expected result", {
   expect_equal_to_reference(fit_meta_d_MLE(nR_S1, nR_S2,
                                            add_constant = FALSE),
                             file = "met_mleNoConst.rds")
+
 })
 
+test_that("meta_d_SSE gives expected results", {
+
+  nR_S1 <- c(29, 33, 12, 7)
+  nR_S2 <- c(3, 15, 21, 43)
+
+  expect_equal_to_reference(fit_meta_d_SSE(nR_S1, nR_S2),
+                            file = "met_sseConst.rds")
+
+  expect_equal_to_reference(fit_meta_d_SSE(nR_S1, nR_S2,
+                                           add_constant = FALSE),
+                            file = "met_sseNoConst.rds")
+
+})
